cf42d9278c5aa6eb0f51a90e6b355cb66a899758
UTurista <vascko@sapo.pt>
2017-05-01 11:58:45 +0100

    Implemented "Add Player" command

--- GSEmulator/src/Commands/AddPlayerCommand.cs
+++ GSEmulator/src/Commands/AddPlayerCommand.cs
@@ -3,18 +3,27 @@ using System;
 
 namespace GSEmulator.Commands
 {
+
     class AddPlayerCommand : Command
     {
         private string[] tokens;
+        private string mName;
+        private uint mPid;
+        private int mPlayerIdx;
 
+        // player_a \00 <id> \00 <name> \00 <pid> \0A
         public AddPlayerCommand(string[] tokens)
         {
             this.tokens = tokens;
+            mPlayerIdx = Int32.Parse(tokens[1]);
+            mName = tokens[2];
+            mPid = UInt32.Parse(tokens[3]);
         }
 
         public override void Execute(ref Server server)
         {
-            throw new NotImplementedException();
+            Player player = new Model.Player(mName, mPid);
+            server.GetPlayers()[mPlayerIdx] = player;
         }
 
     }
