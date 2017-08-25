4993e397940f75e89985e6346f7218307f33f3b0
UTurista <vasckko@gmail.com>
2017-08-25 17:17:35 +0100

    Added some documentation

--- GSEmulator/src/GSProtocol3/Encoder.cs
+++ GSEmulator/src/GSProtocol3/Encoder.cs
@@ -14,6 +14,7 @@ namespace GSEmulator.GSProtocol3
         private Message mMessage;
         private byte[] mTimeStamp;
 
+
         public Encoder(Server server, byte[] timestamp)
         {
             mMessages = new List<Message>();
@@ -22,6 +23,15 @@ namespace GSEmulator.GSProtocol3
 
         }
 
+        /// <summary>
+        /// Encodes the server associated with this encoder into the proper GameSpy format.
+        /// 
+        /// The encoding process allows selecting which information should be included, server headers, players and team information.
+        /// </summary>
+        /// <param name="headers">If true, server headers will be encodend in the message</param>
+        /// <param name="players">If true, players information will be encodend in the message</param>
+        /// <param name="teams">If true, teams information will be encodend in the message</param>
+        /// <returns>A list of messages representing the gamespy encoded messages</returns>
         public List<Message> Encode(bool headers, bool players, bool teams)
         {
             newMessage();
@@ -69,10 +79,19 @@ namespace GSEmulator.GSProtocol3
             mMessage = new Message(mTimeStamp);
         }
 
+        /// <summary>
+        /// Encodes the server's header into gamespy format:
+        ///  0x00 <key 1> 0x00 <value 1> 0x00 ... <key N> 0x00 <value N> 0x00  0x00
+        /// 
+        /// Where the first byte, 0x00, indicates we're handling with the server's headers
+        ///
+        /// </summary>
+        /// <param name="headerIdx">The header's index to start enconding by</param>
+        /// <returns>true if all headers were encoded else false</returns>
         private bool EncodeHeaders(ref int headerIdx)
         {
             LOGGER.Debug("EncodeHeaders");
-            mMessage.put((byte)Message.TYPE_HEADERS);
+            mMessage.put((byte)Message.TYPE_HEADERS);                               // We indicate we're writing server's headers                     
 
             for (; headerIdx < Server.NumFields; headerIdx++)
             {
@@ -80,19 +99,28 @@ namespace GSEmulator.GSProtocol3
                 var value = mServer.GetField(headerIdx);
                 LOGGER.Trace(" {0} => {1}", field, value);
 
-                if (mMessage.put(field) != field.Length || mMessage.put(0) != 1)
+                if (mMessage.put(field) != field.Length || mMessage.put(0) != 1)    // We attempt to write the key and terminate it with 0x00
                     return false;
 
-                if (value != null && mMessage.put(value) != value.Length)
+                if (value != null && mMessage.put(value) != value.Length)           // We attempt to write the value
                     return false;
 
-                if (mMessage.put(0) != 1)
+                if (mMessage.put(0) != 1)                                           // we attempt to terminate the value with 0x00
                     return false;
             }
 
-            return mMessage.put(0) == 1;
+            return mMessage.put(0) == 1;                                            // All headers written, we terminate it with 0x00
         }
 
+        /// <summary>
+        /// Encodes the player's information into gamespy format:
+        ///  0x01 <key 1> 00 <offset> <value 1 + offset> 0x00 ... <value NN> 0x00 0x00 <key 2> 0x00 <offset> <value 1 + offset> 0x00 ... <value NN> 0x00 0x00 0x00
+        /// 
+        /// Where the first byte, 0x01, indicates we're handling with the player's information
+        /// </summary>
+        /// <param name="playerIdx"></param>
+        /// <param name="fieldIdx"></param>
+        /// <returns></returns>
         private bool EncodePlayers(ref int playerIdx, ref int fieldIdx)
         {
             LOGGER.Debug("EncodePlayers");
@@ -100,37 +128,38 @@ namespace GSEmulator.GSProtocol3
 
             var players = mServer
                             .GetPlayers()
-                            .Where((player) => player != null) // We convert our sparced array into a dense one
+                            .Where((player) => player != null)                      // We convert our sparced array into a dense one
                             .ToArray(); 
 
             for (; fieldIdx < Player.NumFields; playerIdx = 0, fieldIdx++)
             {
                 var field = Player.GetFieldName(fieldIdx);
 
-                if (mMessage.put(field) != field.Length || mMessage.put(0) != 1)
+                if (mMessage.put(field) != field.Length || mMessage.put(0) != 1)    // We attempt to write the key and terminate it with 0x00
                     return false;
 
-                if (mMessage.put((byte)playerIdx) != 1)
+                if (mMessage.put((byte)playerIdx) != 1)                             // We attempt to write the value index
                     return false;
 
 
-                for (; playerIdx < players.Length; playerIdx++)
+                for (; playerIdx < players.Length; playerIdx++)                       
                 {
-                    var value = players[playerIdx].GetField(fieldIdx);
+                    var value = players[playerIdx].GetField(fieldIdx);              
 
-                    if (value != null && mMessage.put(value) != value.Length)
+                    if (value != null && mMessage.put(value) != value.Length)       // We attempt to write the value
                         return false;
 
-                    if (mMessage.put(0) != 1)
+                    if (mMessage.put(0) != 1)                                       // And terminate it with 0x00
                         return false;
                 }
 
-                if (mMessage.put(0) != 1)
+                if (mMessage.put(0) != 1)                                           // All values for this key are written, we terminate it with 0x00
                     return false;
             }
-            return mMessage.put(0) == 1;
+            return mMessage.put(0) == 1;                                            // All values for all keys are written, we terminate it with 0x00
         }
 
+        //TODO: EncodeTeams is the same as Encodedlayers! Refactor to make a single method
         private bool EncodeTeams(ref int teamIdx, ref int fieldIdx)
         {
             LOGGER.Debug("EncodeTeams");
--- GSEmulator/src/GSProtocol3/Message.cs
+++ GSEmulator/src/GSProtocol3/Message.cs
@@ -6,6 +6,19 @@ using NLog;
 
 namespace GSEmulator.GSProtocol3
 {
+    /// <summary>
+    /// A representation of a gamespy query's reply.
+    /// 
+    /// The message has the follow structure:
+    ///  - 0x00 XX XX XX XX splitnum 0x00 YY PAYLOAD
+    /// 
+    /// Where:
+    /// - The first 4 bytes are a timestamp, provided by the query
+    /// - splitnum is that byte array representation of "splitnum"
+    /// - YY is the 15th byte  and has dual information:
+    ///   - The 7 most significat bits indicate the index of the message
+    ///   - The least significat bit indicates if this is the last message
+    /// </summary>
     class Message
     {
         private static Logger LOGGER = LogManager.GetCurrentClassLogger();
@@ -48,11 +61,14 @@ namespace GSEmulator.GSProtocol3
             put(0);
         }
 
-
+        /// <summary>
+        /// Defines the index of this message. 
+        /// </summary>
         public byte Index
         {
             get
             {
+                // As per documentation the index is stored in the 7 most significant bits of the 15th byte
                 return (byte)(mPack[14] & 0x7f);
             }
             set
@@ -64,12 +80,15 @@ namespace GSEmulator.GSProtocol3
             }
         }
 
+        /// <summary>
+        /// Defines if this is or not the last message.    
+        /// </summary>
         public bool IsLast
         {
             get
             {
-                bool isLast = (mPack[14] & 0x80) == 0x80;
-                return isLast;
+                // As per documentation the index is stored in the least significant bit of the 15th byte
+                return (mPack[14] & 0x80) == 0x80;
             }
             set
             {
@@ -82,6 +101,11 @@ namespace GSEmulator.GSProtocol3
             }
         }
 
+        /// <summary>
+        /// Writes a single byte to the buffer and increments the position by 1;
+        /// </summary>
+        /// <param name="value">byte to be written in the buffer</param>
+        /// <returns>1 if the byte was writen else it returns 0</returns>
         public int put(byte value)
         {
             if (mPosition < MAX_SIZE) {
@@ -92,6 +116,11 @@ namespace GSEmulator.GSProtocol3
             return 0;
         }
 
+        /// <summary>
+        /// Writes the byte array representation of the given string to the buffer and increments the position accordingly.
+        /// </summary>
+        /// <param name="value">String to be written in the buffer</param>
+        /// <returns>The number of written bytes</returns>
         public int put(string value)
         {
             int written = 0;
@@ -107,6 +136,12 @@ namespace GSEmulator.GSProtocol3
             return written;
         }
 
+
+        /// <summary>
+        /// Writes the byte array to the buffer and increments the position accordingly.
+        /// </summary>
+        /// <param name="bytes">byte array to be written in the buffer</param>
+        /// <returns>The number of written bytes</returns>
         public int put(byte[] bytes)
         {
             int written = 0;
@@ -120,6 +155,12 @@ namespace GSEmulator.GSProtocol3
             return written;
         }
 
+        /// <summary>
+        /// Returns a trimmed byte array representation of this message.
+        /// 
+        /// It also guarantees that the 1400th byte is always zero.
+        /// </summary>
+        /// <returns>Returns a trimmed byte array representation of this message.</returns>
         public byte[] GetBytes()
         {
             mPack[MAX_SIZE - 1] = 0;
